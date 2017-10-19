11a6bc4e1e8ab3f003e3349e86bdac151c291f54
asoler91 <32654790+Asoler19@users.noreply.github.com>
2017-10-19 15:44:58 -0700

    Test 2 Completed

---
+++ Q2.js
@@ -0,0 +1,19 @@
+// In only one console log, use the + operator to concatenate the following values.
+// Each value must be separated by a space.
+
+"eight"
+5
+"six"
+"two"
+3
+7
+
+var op1 = 'eight';
+var op2 = 5;
+var op3 = 'six';
+var op4 = 'two';
+var op5 = 3;
+var op6 = 7;
+
+console.log('eight' + " " + 5 + " " + 'six' + " " + 'two' + " " + 3 + " " + 7);
+
---
+++ Q4.js
@@ -0,0 +1,6 @@
+// Console log any expression that uses the modulo operator % and results
+// in the number 1.
+
+var op = 5%2;
+
+console.log(op);
